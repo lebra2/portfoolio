15ad92530afe8679ce12192f38978bc5db419ebd
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-11-17 19:49:12 +0200

    asdf

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '1/3': '33.33333%',
+        '2/3': '66.66667%',
+      }
+
+      ,colors: {
+        spotify: '#1DB954',
+        soundcloud: '#ff7700',
+        youtube: '#FF0000',
+        ig: '#3f729b',
+      }
+    },
   },
   plugins: [],
 }
