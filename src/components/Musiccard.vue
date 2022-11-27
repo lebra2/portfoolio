d5b62a871c9c64616de3b7d6316e4566de47ce49
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-11-27 23:43:50 +0200

    fixes

--- src/components/Musiccard.vue
+++ src/components/Musiccard.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex justify-center sm:flex-row sm:bg-opacity-0 text-white items-center bg-white bg-opacity-5 m-4 flex-col md:mt-32 md:mb-32 gap-12 p-8">
         <div class="w-[300px] lg:w-[400px]">
-            <a class="" href="https://open.spotify.com/track/0NogQwG9HA6qcxzVWg2oO5?si=7bb4c0ae956544e7" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/Mina/My project-1(2).png" alt=""></a>
+            <a class="" href="https://open.spotify.com/track/0NogQwG9HA6qcxzVWg2oO5?si=7bb4c0ae956544e7" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/Mina/EiSaa.png" alt=""></a>
             <div class=" py-4">
                 <h1 class="mt-4 text-center text-3xl font-semibold">Single</h1>
                 <h2 class="mt-4 text-rose-500 text-center text-2xl font-semibold">"Ei Saa"</h2>
             </div>
         </div>
         <div class="w-[300px] lg:w-[400px]">
-            <a class="" href="https://open.spotify.com/album/0OezEYmpVy2dfkTzi7JC3O?si=9-oweloRRQqeSmDpmBVp4w" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/My project-1(1).jpg" alt=""></a>
+            <a class="" href="https://open.spotify.com/album/0OezEYmpVy2dfkTzi7JC3O?si=9-oweloRRQqeSmDpmBVp4w" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/Sulan.jpg" alt=""></a>
             <div class=" py-4">
                 <h1 class="mt-4 text-center text-3xl font-semibold">Latest Single</h1>
                 <h2 class="mt-4 text-rose-500 text-center text-2xl font-semibold">"Sulan"</h2>
