32c338e592faf432c602fd999d2c5c78e45efe17
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-11-27 23:37:47 +0200

    punkt

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Musiccard from './components/Musiccard.vue';
 </script>
 
 <template>
-  <img src="./assets/Pictures/Mina/My project-1(3).png" alt="taust" id="bgImage" class="fixed h-screen w-screen -z-50" style="object-fit: cover; filter: brightness(0.5) blur(10px) saturate(0.5); scale: 1.5;">
+  <img src="../assets/Pictures/Mina/My project-1(3).png" alt="taust" id="bgImage" class="fixed h-screen w-screen -z-50" style="object-fit: cover; filter: brightness(0.5) blur(10px) saturate(0.5); scale: 1.5;">
   
   <div class="">
     <RouterView />
