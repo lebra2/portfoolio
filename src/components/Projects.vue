c97657ecf8767908bee8a2ee99fde1538c9bcaac
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-12-05 23:14:10 +0200

    rev

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,17 +1,5 @@
 <template>
     <div class="gap-12 grid justify-center mx-8 sm:grid-cols-2 md:grid-cols-3 my-12">
-        <router-link to="/Project2">
-            <div class="">
-                <div class="">
-                    <img class=" rounded-t-lg" src="../assets/Pictures/Väikekannel.jpg" alt="">
-                </div>
-                <div class="bg-white h-full p-4 rounded-b-lg">
-                    <h1 class="mb-4 font-bold sm:text-3xl text-2xl md:text-5xl">3. projekt</h1>
-                    <p class="sm:hidden text-xl">Esimene projekt</p>
-                    <p class="text-xl font-semibold">Kolmas projekt praegu on jalutama lainud, varsti lisan selle siia, kui leian vajalikud failid</p>
-                </div>
-            </div>
-        </router-link>
         <router-link to="/Project3">
             <div class="">
                 <div class="">
