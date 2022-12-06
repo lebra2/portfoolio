5620bf0589c8574117ba216800190c5dec52ba47
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-12-06 12:21:21 +0200

    ceevee

--- src/components/ceevee.vue
+++ src/components/ceevee.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 p-12 mx-auto md:mx-48 rounded-2xl my-2">
-        <embed src="/assets/kadri/eneseareng.pdf" class="w-full h-96" type="application/pdf">
-        <embed src="/assets/kadri/iseloom1.pdf" class="w-full h-96" type="">
-        <embed src="/assets/kadri/iseloom2.pdf" class="w-full h-96" type="">
-        <embed src="/assets/kadri/isikomad2.pdf" class="w-full h-96" type="">
-        <embed src="/assets/kadri/mina.pdf" class="w-full h-96" type="">
-        <embed src="/assets/kadri/mindmap.PNG" class="w-full h-96" type="">
-        <embed src="/assets/kadri/Opioskused.pdf" class="w-full h-96" type="">
-        <embed src="/assets/kadri/tooalane.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/eneseareng.pdf" class="w-full h-96" type="application/pdf">
+        <embed src="../src/assets/kadri/iseloom1.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/iseloom2.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/isikomad2.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/mina.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/mindmap.PNG" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/Opioskused.pdf" class="w-full h-96" type="">
+        <embed src="../src/assets/kadri/tooalane.pdf" class="w-full h-96" type="">
     </div>
 </template>
