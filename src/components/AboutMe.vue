fed326b02b28582195fff7fe2474bdb379995865
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-01-11 14:54:51 +0200

    changes portfoolio

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,22 +1,26 @@
 <template> 
 
-    <div class="h-full lg:mx-24 my-12">
+    <div class="h-full my-12">
         <div class="text-center md:text-left h-full mx-4">
-            <h1 class="text-white text-center text-5xl md:text-6xl font-bold">Minu portfoolio</h1>
-            <div class="mt-8 rounded p-4 text-2xl flex flex-col gap-4">
+            <div class="mt-8 rounded p-4 text-xl flex flex-col gap-4 bg-black bg-opacity-50 md:mx-48 md:p-4">
                 <p class="text-white text-left font-semibold">Olen 17-aastane noorem tarkvaraarendaja ja õpin Kuressaare Ametikoolis. Oskan juba html-i ja css-iga lihtsamaid veebilehti teha ning olen õppinud veel Pythonit, Javascripti, Mysqli, linuxi terminali jne.
                     Ma arvan et ma olen sõbralik, seltskondlik ja avatud inimene, oskan inimestega suhelda ja leian kiiresti sõpru.
                     Vabal ajal tegelen muusika loomisega ning minu eesmärk on ära õppida tulevikus C++, et teha oma pluginaid, mida müüa.</p>
+                    <div class="flex gap-2">
+                        <router-link to="/ceevee">
+                            <div class="flex">
+                                <button class="text-white bg-black bg-opacity-50 w-8 h-8 rounded-full">CV</button>
+                            </div>
+                        </router-link>
+                            <div class="flex">
+                                <a href="https://github.com/lebra2"><button class="text-white bg-white w-8 h-8 rounded-full"><img src="../assets/Pictures/github-logo(1).png" alt=""></button></a>
+                            </div>
+                    </div>
+
             </div>
+            
             
         </div>
 
     </div>
-
-    <router-link to="/ceevee">
-        <div class="flex mx-8">
-            <button class="text-white bg-green-500 w-12 h-12 rounded-full">CV</button>
-        </div>
-
-    </router-link>
 </template>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,27 +1,21 @@
 <template>
-    <div class="gap-12 grid justify-center mx-8 sm:grid-cols-2 my-12">
-        <router-link to="/Project3">
-            <div class="w-full h-48">
-                <div class="">
-                    <img class="rounded-t-lg" src="../assets/Pictures/Väikekannel.jpg" alt="">
-                </div>
-                <div class="bg-white h-full p-4 rounded-b-lg">
-                    <h1 class="mb-4 font-bold sm:text-3xl text-2xl md:text-5xl">V-Instrument</h1>
-                    <p class="sm:hidden text-xl">Teine projekt</p>
-                    <p class="text-xl font-semibold">Teine projekt oli vaja teha virtuaalne instrument milleks oli kannel. Edasi saad lugeda. lalalalalal.</p>
-                </div>
+    <div class="h-screen gap-16 grid mx-8 sm:grid-cols-2 my-12 md:mx-48">
+        <router-link to="/Project1">
+            <div class="bg-black bg-opacity-50 text-center rounded-lg">
+                <h1 class="text-white p-4 font-semibold">Miki hiirest portfoolio tegemine</h1>
+                <img src="../assets/Pictures/micki.png" alt="">
             </div>
         </router-link>
         <router-link to="/Project1">
-            <div class="">
-                <div class="">
-                    <img class="rounded-t-lg" src="../assets/Pictures/micki.png" alt="">
-                </div>
-                <div class="bg-white h-full p-4 rounded-b-lg">
-                    <h1 class="mb-4 font-bold sm:text-3xl text-2xl md:text-5xl">E-portfoolio</h1>
-                    <p class="sm:hidden text-xl">Esimene projekt</p>
-                    <p class="text-xl font-semibold">Esimene projekt oli vaja teha portfoolio väljamõeldud tegelasest, meie valitud tegelaseks sai Miki Hiir...</p>
-                </div>
+            <div class="bg-black bg-opacity-50 text-center rounded-lg">
+                <h1 class="text-white p-4 font-semibold">Virtuaalne instrument kannel</h1>
+                <img src="../assets/Pictures/Väikekannel.jpg" alt="">
+            </div>
+        </router-link>
+        <router-link to="/Project1">
+            <div class="bg-black bg-opacity-50 text-center rounded-lg">
+                <h1 class="text-white p-4 font-semibold">Noor Meister 2022</h1>
+                <img src="../assets/Pictures/noor-meister-2022.jpg" alt="">
             </div>
         </router-link>
     </div>
