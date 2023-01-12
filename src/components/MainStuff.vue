27fc0aa310fadd7844957fed4fb479c6cecddd0d
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-01-12 09:58:04 +0200

    diskograafia

--- src/components/MainStuff.vue
+++ src/components/MainStuff.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="font-['League'] bg-black bg-opacity-50 md:h-auto md:grid flex flex-col md:grid-cols-2 xl:mx-12 2xl:mx-24 p-4 lg:my-12 lg:gap-24">
+    <div class="font-['League'] bg-black bg-opacity-50 md:h-auto md:grid flex flex-col md:grid-cols-2 xl:mx-12 2xl:mx-48 p-4 lg:my-12 lg:gap-24">
         
         <div class=" h-full">
             <div class="">
@@ -7,7 +7,7 @@
             </div>
             <div class="gap-2 text-white text-2xl flex justify-center flex-col items-center">
                 <h1 class="text-4xl">Mis on usaldus?</h1>
-                <h2 class="font-semibold text-red-500 text-5xl">12.01</h2>
+                <h2 class="font-semibold text-red-500 text-5xl">NÜÜD ÜLEVAL!</h2>
             </div>
             <div class="gap-4 p-4 text-white text-2xl flex justify-center flex-col items-center">
                 <a class="bg-white bg-opacity-5  rounded-full p-2 text-spotify" href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA">Spotify</a>
@@ -17,7 +17,7 @@
 
 
         </div>
-        <div class="mt-4 text-white text-center shadow-md bg-white bg-opacity-5 p-8">
+        <div class="mt-4 text-white text-center shadow-md bg-white bg-opacity-5 lg:p-8">
             <div class="bg-black bg-opacity-25 p-2">
                 <img class="h-96 w-full p-4 rounded-lg" style="object-fit:cover" src="../assets/Pictures/1656201069095(1).jpg" alt="">
                 <h1 class="text-4xl font-semibold my-4 font-['South']">LEBRA</h1>
--- src/views/Disko.vue
+++ src/views/Disko.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex justify-center sm:flex-row sm:bg-opacity-0 text-white items-center m-4 flex-col md:mt-32 md:mb-32 gap-4">
-        <div class="w-[300px] lg:w-[400px]">
+    <div class="flex justify-center sm:flex-row bg-black bg-opacity-50 text-white items-center m-4 flex-col md:my-32 gap-4">
+        <div class="w-[300px] lg:w-[400px] mt-4">
             <a class="" href="https://open.spotify.com/album/0OezEYmpVy2dfkTzi7JC3O?si=9-oweloRRQqeSmDpmBVp4w" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/Sulan.jpg" alt=""></a>
             <div class=" py-4 flex flex-col gap-4">
                 <h1 class="text-center text-3xl font-semibold">Latest Single</h1>
@@ -15,10 +15,59 @@
             </div>
         </div>
         <div class="w-[300px] lg:w-[400px]">
-            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/lainetel(1).png" alt=""></a>
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/hingekaaslane.png" alt=""></a>
             <div class=" py-4 flex flex-col gap-4" >
                 <h1 class="text-center text-3xl font-semibold">Single</h1>
-                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Lainetel"</h2>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Hingekaaslane"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/lahti.jpg" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Lahti Lasta"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/Lõpp(1).png" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Lõpp"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/kuisindnaen.jpg" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Kui Sind Näen"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/sedööver.png" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Šedööver"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/vöörad.png" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Vöörad"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/ajad.jpg" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Ajad me ees"</h2>
+            </div>
+        </div>
+        <div class="w-[300px] lg:w-[400px]">
+            <a class="" href="https://open.spotify.com/album/3R9CciNdvR4nhDE2kahkrY?si=xYMS-OPiRJiJjY3zTjQDxw" target="_blank"><img class="flex md:hover:scale-105 trans035 rounded" src="../assets/Pictures/allee.png" alt=""></a>
+            <div class=" py-4 flex flex-col gap-4" >
+                <h1 class="text-center text-3xl font-semibold">Single</h1>
+                <h2 class="text-rose-500 text-center text-2xl font-semibold">"Unistuste Allee"</h2>
             </div>
         </div>
     </div>
