083708ecc9f84e494bdd06a294fce6ec7d063703
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-01-31 12:04:53 +0200

    asd

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,13 +3,11 @@
         <nav id="nav" class="sticky bg-black bg-opacity-50 md:bg-opacity-0 top-0 z-50 md:z-1 md:flex md:items-center md:justify-between">
             <div class="flex flex-row justify-between sticky ">
                 <div class="flex font-['South']">
-                    <router-link to="/" class="flex text-white p-4 text-4xl font-bold sm:ml-4">
-                        LEBRA
-                    </router-link>
+                    <a href="https://www.lebra.ee/" class="flex text-white p-4 text-4xl font-bold sm:ml-4">LEBRA</a>
 
                 </div>
             </div>
-                <ul id="" class="sm:mr-4 md:items-center md:flex z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+                <!-- <ul id="" class="sm:mr-4 md:items-center md:flex z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
                     <li class="mx-4 my-6 md:my-0">
                         <router-link to="/" class="text-xl hover-underline-animation text-white hover:text-red-600 trans02">
                             Kodu
@@ -26,7 +24,7 @@
                         </router-link>
                     </li>
                     
-                </ul>
+                </ul> -->
         </nav>
           
 
