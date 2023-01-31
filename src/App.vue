60bc02a579582a307bb414ba57aa8d803e608677
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-01-31 10:58:00 +0200

    Portfoolio

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 
 import Navigation from './components/Navigation.vue'
-import Mainfooter from './components/Mainfooter.vue';
-import Musiccard from './components/Musiccard.vue';
 
 </script>
 
