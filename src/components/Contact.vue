df47b5ae9053c1f85f0e7d53650e38664ea6fffb
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-03 22:51:25 +0200

    asdf

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,12 +6,12 @@ const props = defineProps({
 </script>
 
 <template>
-    <div id="Contact" class="h-screen bg-[#080808] text-white font-['Jetbrains'] flex justify-center items-center text-2xl lg:text-4xl flex-col">
-        <h1 class="my-8">Kontakteeru</h1>
+    <div id="Contact" class="h-screen bg-[#080808] text-white font-['Jetbrains'] flex justify-center text-2xl items-center flex-col">
+        <h1 class="my-8 lg:text-4xl">Kirjuta mulle</h1>
         <form action="https://tak21timofejev.itmajakas.ee" method="post">
             <div class="">
                 <div>
-                    <p class="">Nimi</p>
+                    <p class="lg:text-2xl">Nimi</p>
                     <input type="text" name="name" required="required" class="bg-[#282828]">
                 </div>
                 <div>
@@ -30,6 +30,26 @@ const props = defineProps({
 
             </div>
         </form>
+        <div class="font-['Jetbrains'] flex mt-4 gap-4 ">
+            <h1></h1>
+            <div class="rounded-md">
+                <img class="w-12 p-2 h-12 bg-white rounded-full" src="../assets/Pictures/github-logo(1).png" alt=""><a href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA"></a>
+            </div>
+            <div class="rounded-md">
+                <img class="w-12 p-2 h-12 bg-white rounded-full" src="../assets/Pictures/github-logo(1).png" alt=""><a href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA"></a>
+            </div>
+            <div class="rounded-md">
+                <img class="w-12 p-2 h-12 bg-white rounded-full" src="../assets/Pictures/github-logo(1).png" alt=""><a href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA"></a>
+            </div>
+            <div class="rounded-md">
+                <img class="w-12 p-2 h-12 bg-white rounded-full" src="../assets/Pictures/github-logo(1).png" alt=""><a href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA"></a>
+            </div>
+            <div class="rounded-md">
+                <img class="w-12 p-2 h-12 bg-white rounded-full" src="../assets/Pictures/github-logo(1).png" alt=""><a href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA"></a>
+            </div>
+        </div>
     </div>
+    
+
 
 </template>
