cf76f3da4cbacdd322a8fe0d8017114d14fd17d2
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-04 20:15:30 +0200

    why

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
-  <img class="fixed h-screen w-screen -z-50" src="../assets/Pictures/kiilakas.png" alt="" style="object-fit: cover; filter: brightness(1) blur(5px) saturate(0.5); scale: 1.1;">
-  
   <div class="">
     <RouterView />
   </div>
