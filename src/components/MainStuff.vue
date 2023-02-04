8f0e5bd11a2cc371ec4a3464c7578fa8817736c4
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-04 19:09:27 +0200

    test

--- src/components/MainStuff.vue
+++ src/components/MainStuff.vue
@@ -1,81 +1,4 @@
-
 <template>
-    <div class="h-[100vh] lg:justify-center flex flex-col py-24 items-center lg:px-24 lg:mx-24 2xl:mx-48 2xl:px-48">
-        <h1 class="text-white text-4xl">Diskograafia</h1>
-      <div class="flex mt-10 gap-4">
-        <button class="text-gray-500 font-bold py-2 rounded-full" @click="prevSong">
-          <p>Prev</p>
-        </button>
-        <img :src="currentSong.image" class="w-64 h-64 object-cover rounded-lg"/>
-        <button class=" text-gray-500 font-bold py-2 rounded-full" @click="nextSong">
-            <p>Next</p>
-        </button>
-      </div>
-      <div class="dropdown mt-10 ">
-        <select class="bg-[#212121] text-white border border-gray-400 py-2 px-4" v-model="selectedSongIndex">
-          <option v-for="(song, index) in songs" :key="index" :value="index">
-            {{ song.title }}
-          </option>
-        </select>
-      </div>
-      <p class="text-xl text-second font-medium mt-10">{{ currentSong.title }} ({{ currentIndex + 1 }}/{{ songs.length }})</p>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted, watch } from 'vue'
-  
-  export default {
-    setup() {
-      const songs = [
-        { title: 'Mis on usaldus', image: '../assets/Pictures/usaldus.png' },
-        { title: 'Ei Saa', image: 'src/assets/Pictures/EiSaa.png' },
-        { title: 'Hingekaaslane', image: '../assets/Pictures/hingekaaslane.png' },
-        { title: 'Lahti Lasta', image: '../assets/Pictures/lahti.jpg' },
-        { title: 'Lõpp', image: '../assets/Pictures/Lõpp(1).png' },
-        { title: 'Kui Sind Näen', image: '../assets/Pictures/kuisindnaen.jpg' },
-        { title: 'Šedööver', image: '../assets/Pictures/sedööver.png' },
-        { title: 'Vöörad', image: '../assets/Pictures/vöörad.png' },
-        { title: 'Ajad me ees', image: '../assets/Pictures/ajad.jpg' },
-        { title: 'Unistuste Allee', image: '../assets/Pictures/allee.png' },
-      ]
-      const currentIndex = ref(0)
-      const currentSong = ref(songs[currentIndex.value])
-      const selectedSongIndex = ref(0)
-  
-      function prevSong() {
-        currentIndex.value = (currentIndex.value + songs.length - 1) % songs.length
-        currentSong.value = songs[currentIndex.value]
-      }
-  
-      function nextSong() {
-        currentIndex.value = (currentIndex.value + 1) % songs.length
-        currentSong.value = songs[currentIndex.value]
-      }
-  
-      watch(() => selectedSongIndex.value, (index) => {
-        currentIndex.value = index
-        currentSong.value = songs[currentIndex.value]
-      })
-  
-      onMounted(() => {
-        currentSong.value = songs[currentIndex.value]
-      })
-  
-      return {
-        songs,
-        currentIndex,
-        currentSong,
-        prevSong,
-        nextSong,
-        selectedSongIndex
-      }
-    }
-  }
-  </script>
-
-
-<!-- <template>
     <div class="font-['Jetbrains'] h-screen flex flex-col justify-center items-center p-4 ">
         <div class=" h-full pt-24 lg:pt-0">
             <div class="flex justify-center items-center lg:pt-16">
@@ -93,4 +16,4 @@
         </div>
     </div>
 
-</template> -->
+</template>
