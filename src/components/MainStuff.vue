7e1dd6418b74df423af19d1ff4ef427be78b13a7
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-04 15:00:37 +0200

    ye

--- src/components/MainStuff.vue
+++ src/components/MainStuff.vue
@@ -1,45 +1,22 @@
 <template>
-    <div class="font-['Jetbrains'] md:grid flex flex-col md:grid-cols-2 xl:mx-12 2xl:mx-48 p-4 lg:gap-24">
+    <div class="font-['Jetbrains'] flex flex-col md:grid-cols-2 xl:mx-12 2xl:mx-48 p-4 lg:gap-24">
         <div class=" h-full">
-            <div class="">
-                <a class="" href="https://open.spotify.com/album/449Zm5tF0XMRI5Rz6EBmJ3?si=r534OT6eSmW_syQM8yntsQ" target="_blank"><img class="flex trans035 rounded p-8" src="../assets/Pictures/usaldus.png" alt=""></a>
-            </div>
-            <div class="gap-2 text-white text-2xl flex justify-center flex-col items-center">
-                <h1 class="text-4xl">Mis on usaldus?</h1>
-                <h2 class="font-semibold text-red-500 text-5xl text-center">NÜÜD ÜLEVAL!</h2>
-            </div>
-            <div class="gap-4 p-4 text-white text-2xl flex justify-center flex-col items-center">
-                <a class="bg-white bg-opacity-5 rounded-full p-2 text-spotify" href="https://open.spotify.com/album/449Zm5tF0XMRI5Rz6EBmJ3?si=r534OT6eSmW_syQM8yntsQ">Spotify</a>
-                <a class="bg-white bg-opacity-5 rounded-full p-2 text-youtube" href="https://www.youtube.com/watch?v=MZp_g3Vx_zY">Youtube</a>
-                <a class="bg-white bg-opacity-5 rounded-full p-2 text-soundcloud" href="https://soundcloud.com/lebraofficial/lebra-mis-on-usaldus">Soundcloud</a>
-            </div>
-            <div class="flex justify-center">
+            <div class="flex justify-center px-8 md:hidden">
                 <iframe width="500" height="315" src="https://www.youtube.com/embed/MZp_g3Vx_zY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
+            <div class="flex justify-center hidden md:block items-center lg:px-48 lg:pt-48 px-8">
+                <a class="" href="https://open.spotify.com/album/449Zm5tF0XMRI5Rz6EBmJ3?si=r534OT6eSmW_syQM8yntsQ" target="_blank"><img class="w-full lg:h-[32rem]" src="../assets/Pictures/usaldus.png" alt=""></a>
+            </div>
+            <div class="gap-2 text-white text-2xl flex justify-center flex-col items-center">
+                <h1 class="text-2xl mt-4 text-second">Mis On Usaldus?</h1>
+                <h2 class="font-extrabold text-red-500 text-4xl text-center">NÜÜD ÜLEVAL!</h2>
+            </div>
+            <div class="gap-4 p-4 text-white text-sm flex xl:flex-row justify-center flex-col items-center">
+                <a class="bg-white bg-opacity-5 rounded-md p-1" href="https://open.spotify.com/album/449Zm5tF0XMRI5Rz6EBmJ3?si=r534OT6eSmW_syQM8yntsQ">Spotify</a>
+                <a class="bg-white bg-opacity-5 rounded-md p-1" href="https://www.youtube.com/watch?v=MZp_g3Vx_zY">Youtube</a>
+                <a class="bg-white bg-opacity-5 rounded-md p-1" href="https://soundcloud.com/lebraofficial/lebra-mis-on-usaldus">Soundcloud</a>
+            </div>
 
-
-        </div>
-        <div  id="minust" class="mt-4 text-white text-center shadow-md bg-white bg-opacity-5 lg:p-8">
-            <div class="p-2">
-                <img class="h-96 w-full p-4 rounded-lg" style="object-fit:cover" src="../assets/Pictures/1656201069095(1).jpg" alt="">
-                <h1 class="text-4xl font-semibold my-4 font-['South']">LEBRA</h1>
-                <h2 class="text-2xl">Noor Eesti räppartist/produtsent, kelle lood on peamiselt armastusest. <br> Lebra alustas oma muusika avaldamisega 2019. aastal Soundcloudis ning on nüüd jõudnud mitmete tuhandete kuulajateni. Lood on üleval nüüdseks platvormidel nagu Spotify, Youtube, Soundcloud, Apple Music, Deezer ja mujal.</h2>
-            </div>
-            <div class="gap-4 p-4 text-white text-2xl flex justify-center flex-col items-center">
-                <router-link to="/Disko" class="bg-white bg-opacity-50 text-black rounded-full p-2" href="https://open.spotify.com/artist/70PCvEt9nvUsGdQQCjF1re?si=0Sc4UYlIRXSgHlpupE0KHA">Diskograafia</router-link>
-            </div>
-            <iframe class="h-[60vh]" style="border-radius:12px" src="https://open.spotify.com/embed/artist/70PCvEt9nvUsGdQQCjF1re?utm_source=generator&theme=0" width="100%" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            
-            <div id="biidid" class="">
-                <div class="py-4">
-                    <h1 class="text-white text-2xl text-center">Tutvu biitidega</h1>
-                    <a class="text-second" href="https://www.youtube.com/@lbra9456/featured">lebra beats - youtube</a>
-                </div>
-                <div class="flex justify-center">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/24p9K_sB2OE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-                
-            </div>
         </div>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <script setup>
+import About from '../components/About.vue';
 import Navigation from '../components/Navigation.vue';
-import Mainfooter from '../components/Mainfooter.vue';
 import MainStuff from '../components/MainStuff.vue';
 import MobileLinksVue from '../components/MobileLinks.vue';
 import Contact from '../components/Contact.vue';
+
 
 </script>
 
 <template>
     
     <div class="bg-[#181818]">
-        <div class="bg-[#212121]">
+        <div class="bg-black bg-opacity-25">
             <Navigation />
-        </div>
-
             <MainStuff/>
+        </div>
+            <About/>
             <Contact/>
             <MobileLinksVue/>
-        <Mainfooter />
     </div>
 </template>
