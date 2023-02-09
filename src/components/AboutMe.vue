d32ce21c1e4aac15ee6c0174911cbe0ed15c4efd
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-09 10:30:54 +0200

    test

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center py-24 md:py-8">
     <router-link to="/Projects" class="hidden relative mt-24 md:block xl:text-5xl text-2xl font-['Jetbold'] p-1 md:p-4 text-black -inset-0 -skew-y-3 bg-yellow-300 hover:scale-105">Markus</router-link>
   </div>
 <div class="flex justify-center px-8 items-center md:mt-16 lg:mt-24">
   <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-    <div class="flex justify-center items-center mt-24 md:mt-2">
+    <div class="flex justify-center items-center">
       <img class="w-[20rem] rounded-full" src="../assets/Pictures/mina23-min.jpg" alt="">
     </div>
     <div class="rounded gap-2 lg:text-left flex justify-center flex-col">
       <h1 class="text-white lg:text-6xl text-4xl font-semibold">Markus Timofejev</h1>
         <span class="text-white lg:text-xl md:text-xl text-lg">Noorem tarkvaraarendaja / Räppartist</span>
-        <p class="text-white lg:text-xl md:text-xl text-lg">Tegelen front-end arenduse ja muusika loomisega.</p>
+        <p class="text-white lg:text-xl md:text-xl text-lg truncate">Tegelen front-end arenduse ja muusika loomisega.</p>
       
             <div class="flex py-2">
               <router-link to="/Projects" class="rounded-md font-['Jetbold'] mt-2 p-2 text-black hover:text-black bg-yellow-300 hover:scale-105">Projektid</router-link>
