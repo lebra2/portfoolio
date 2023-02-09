7da8df36bf69f9ef13c27511b7b26ae9f46abae7
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-09 10:50:48 +0200

    adsf

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 </script> -->
 
 <template>
-  <img src="../assets/Pictures/back2.png" alt="taust" id="bgImage" class="fixed h-screen w-screen -z-50" style="object-fit: cover; filter: brightness(1) blur(5px) saturate(0.5); scale: 1.5;">
+  <!-- <img src="../assets/Pictures/back2.png" alt="taust" id="bgImage" class="fixed h-screen w-screen -z-50" style="object-fit: cover; filter: brightness(1) blur(5px) saturate(0.5); scale: 1.5;"> -->
   <div>
     <router-view></router-view>
   </div>
