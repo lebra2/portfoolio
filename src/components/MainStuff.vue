797433e499bf976ec6ccc3ef98d23ddef166e5ff
lebra2 <markus.timofejev@ametikool.ee>
2023-07-11 12:22:15 +0300

    yes

--- src/components/MainStuff.vue
+++ src/components/MainStuff.vue
@@ -4,10 +4,10 @@
             <img class="fixed image h-screen w-screen scale-200 -z-50" src="../assets/Pictures/kiilakas.jpg" alt="" style="object-fit: cover; filter: brightness(1) blur(5px) saturate(0.5);">
   
             <div class="flex justify-center items-center">
-                <a class="" href="https://open.spotify.com/album/449Zm5tF0XMRI5Rz6EBmJ3?si=r534OT6eSmW_syQM8yntsQ" target="_blank"><img class="w-[32rem] lg:w-[60vh] lg:h-[60vh]" src="../assets/Pictures/usaldus.jpg" alt="" style="object-fit:cover"></a>
+                <a class="" href="https://www.youtube.com/watch?v=c54uODoCXl8" target="_blank"><img class="w-[32rem] lg:w-[60vh] lg:h-[60vh]" src="../assets/Pictures/hoolid.jpeg" alt="" style="object-fit:cover"></a>
             </div>
             <div class="gap-2 text-white text-2xl flex justify-center flex-col items-center">
-                <h1 class="text-3xl mt-4 text-second">Mis On Usaldus?</h1>
+                <h1 class="text-3xl mt-4 text-second">Hoolid</h1>
                 <h2 class="font-extrabold text-6xl text-center">UP NOW</h2>
             </div>
             <div class="gap-4 p-4 text-white text-xl flex xl:flex-row justify-center flex-col items-center">
@@ -15,7 +15,7 @@
                 <a class="bg-white bg-opacity-5 rounded-md p-1 hover:scale-105 hover:bg-youtube" href="https://www.youtube.com/watch?v=MZp_g3Vx_zY">Youtube</a>
             </div>
             <div class="flex justify-center items-center">
-                <h1 class="text-white">Lugu usaldusest</h1>
+                <h1 class="text-white">Breakup type song :D</h1>
             </div>
         </div>
     </div>
