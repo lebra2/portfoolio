e7d8e5aeb952de5e9684aa0a3503979b25ae183b
lebra2 <markus.timofejev@ametikool.ee>
2023-11-13 23:00:55 +0200

    new song

--- src/components/MainStuff.vue
+++ src/components/MainStuff.vue
@@ -4,18 +4,18 @@
             <img class="fixed image h-screen w-screen scale-200 -z-50" src="../assets/Pictures/kiilakas.jpg" alt="" style="object-fit: cover; filter: brightness(1) blur(5px) saturate(0.5);">
   
             <div class="flex justify-center items-center">
-                <a class="" href="https://www.youtube.com/watch?v=novCAX4vJoQ" target="_blank"><img class="w-[32rem] lg:w-[60vh] lg:h-[60vh]" src="../assets/Pictures/Viinapäev(1).jpg" alt="" style="object-fit:cover"></a>
+                <a class="" href="https://www.youtube.com/watch?v=oWtC29ZQcOw" target="_blank"><img class="w-[32rem] lg:w-[60vh] lg:h-[60vh]" src="../assets/Pictures/NUTTA.jpg" alt="" style="object-fit:cover"></a>
             </div>
             <div class="gap-2 text-white text-2xl flex justify-center flex-col items-center">
-                <h1 class="text-3xl mt-4 text-second">Viinapäev</h1>
+                <h1 class="text-3xl mt-4 text-second">Nutta v Naerda</h1>
                 <h2 class="font-extrabold text-6xl text-center">UP NOW</h2>
             </div>
             <div class="gap-4 p-4 text-white text-xl flex xl:flex-row justify-center flex-col items-center">
-                <a class="bg-white bg-opacity-5 rounded-md p-1 hover:scale-105 hover:bg-spotify" href="https://open.spotify.com/album/7FlfX9DSUyIY3FK36LlJoP?si=_71npP7AQWmLv9OocivlOQ">Spotify</a>
-                <a class="bg-white bg-opacity-5 rounded-md p-1 hover:scale-105 hover:bg-youtube" href="https://www.youtube.com/watch?v=57KpFlKEVtk">Youtube</a>
+                <a class="bg-white bg-opacity-5 rounded-md p-1 hover:scale-105 hover:bg-spotify" href="https://open.spotify.com/album/7HhHWSQnLcnsP5AADpaN21?si=SZWa9SB_RjG0h6jyoMVl5Q">Spotify</a>
+                <a class="bg-white bg-opacity-5 rounded-md p-1 hover:scale-105 hover:bg-youtube" href="https://www.youtube.com/watch?v=oWtC29ZQcOw">Youtube</a>
             </div>
             <div class="flex justify-center items-center">
-                <h1 class="text-white">Viinapäev</h1>
+                <h1 class="text-white">Nutta v Naerda</h1>
             </div>
         </div>
     </div>
